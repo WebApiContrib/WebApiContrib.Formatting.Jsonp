b7e33581ef5b48e95e9bce419ee1981f0ba6feb4
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2012-07-24 10:52:53 +0200

    initial import for JsonpMediaTypeFormatter and sample project

---
+++ src/WebContribContrib.Formatting.Jsonp.SampleWebHost/App_Start/FormatterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Formatting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebApiContrib.Formatting.Jsonp;
+
+namespace WebContribContrib.Formatting.Jsonp.SampleWebHost.App_Start{
+	public class FormatterConfig{
+		public static void RegisterFormatters(MediaTypeFormatterCollection formatters){
+			formatters.Remove(formatters.JsonFormatter);
+			formatters.Insert(0, new JsonpMediaTypeFormatter {
+				SerializerSettings = new JsonSerializerSettings {
+					ContractResolver = new CamelCasePropertyNamesContractResolver()
+				}
+			});
+		}
+	}
+}
---
+++ src/WebContribContrib.Formatting.Jsonp.SampleWebHost/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebContribContrib.Formatting.Jsonp.SampleWebHost.Controllers{
+	public class ValuesController : ApiController {
+		// GET api/values
+		public IEnumerable<string> Get() {
+			return new string[] { "value1", "value2" };
+		} 
+	}
+}
