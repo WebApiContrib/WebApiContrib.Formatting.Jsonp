8885e3a049fca32a20cd6cf0d9c7ead20919eee9
Ryan Riley <ryan.riley@panesofglass.org>
2013-03-05 22:19:11 -0600

    Refactor the formatter to take an existing JSON media type formatter and delegate JSON serialization to that instance.

--- src/WebApiContrib.Formatting.Jsonp.SampleWebHost/App_Start/FormatterConfig.cs
+++ src/WebApiContrib.Formatting.Jsonp.SampleWebHost/App_Start/FormatterConfig.cs
@@ -3,15 +3,20 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using WebApiContrib.Formatting.Jsonp;
 
-namespace WebContribContrib.Formatting.Jsonp.SampleWebHost.App_Start{
-	public class FormatterConfig{
-		public static void RegisterFormatters(MediaTypeFormatterCollection formatters){
-			formatters.Remove(formatters.JsonFormatter);
-			formatters.Insert(0, new JsonpMediaTypeFormatter {
-				SerializerSettings = new JsonSerializerSettings {
-					ContractResolver = new CamelCasePropertyNamesContractResolver()
-				}
-			});
-		}
-	}
+namespace WebContribContrib.Formatting.Jsonp.SampleWebHost.App_Start
+{
+    public class FormatterConfig
+    {
+        public static void RegisterFormatters(MediaTypeFormatterCollection formatters)
+        {
+            var jsonFormatter = formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var jsonpFormatter = new JsonpMediaTypeFormatter(formatters.JsonFormatter);
+            formatters.Insert(0, jsonpFormatter);
+        }
+    }
 }
